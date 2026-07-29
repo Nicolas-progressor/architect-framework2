8593715fc55e5a5342c61e721e1ff9d75d6e7c14
Nicolas-Progressor <progressor@li.ru>
2026-07-29 23:37:46 +0300

    Fix EventServiceProvider: non-existent base class
    EventServiceProvider extended \Architect\Core\ServiceProvider which
    was never created. Changed to extend AbstractServiceProvider and
    use ContainerInterface-based register()/boot() like all other providers.

--- architect/Services/Event/Providers/EventServiceProvider.php
+++ architect/Services/Event/Providers/EventServiceProvider.php
@@ -4,29 +4,34 @@ declare(strict_types=1);
 
 namespace Architect\Services\Event\Providers;
 
+use Architect\Contracts\Core\ContainerInterface;
 use Architect\Contracts\Event\EventDispatcherInterface;
 use Architect\Services\Event\EventManager;
+use Architect\Support\AbstractServiceProvider;
 
-class EventServiceProvider extends \Architect\Core\ServiceProvider
+class EventServiceProvider extends AbstractServiceProvider
 {
-    public function register(): void
+    public function register(ContainerInterface $container): void
     {
-        $this->app->singleton(EventManager::class, function () {
+        $this->registerSingleton($container, EventManager::class, function () {
             return new EventManager();
         });
 
-        $this->app->singleton(EventDispatcherInterface::class, function ($app) {
-            return $app->make(EventManager::class);
+        $this->registerSingleton($container, EventDispatcherInterface::class, function ($c) {
+            return $c->get(EventManager::class);
         });
     }
 
-    public function boot(): void
+    public function boot(ContainerInterface $container): void
     {
-        $eventManager = $this->app->make(EventManager::class);
+        $eventManager = $container->get(EventManager::class);
 
-        if (isset($this->config['events'])) {
-            foreach ($this->config['events'] as $event => $listeners) {
-                foreach ($listeners as $listener) {
+        if ($container->has('config.events')) {
+            $config = $container->get('config.events');
+            $listeners = $config instanceof \ArrayAccess ? $config : ($config['events'] ?? []);
+
+            foreach ($listeners as $event => $eventListeners) {
+                foreach ($eventListeners as $listener) {
                     $priority = is_array($listener) ? ($listener['priority'] ?? 0) : 0;
                     $callable = is_array($listener) ? $listener['handler'] : $listener;
                     $eventManager->listen($event, $callable, $priority);
